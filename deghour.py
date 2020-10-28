d0f704aee3d1f995350f512c8ba1edd7018bbd96
Kevin <kev.y2718@gmail.com>
2020-10-28 20:06:24 +1100

    Done

--- deghour.py
+++ deghour.py
@@ -6,20 +6,40 @@ veraison_start = 1098 #start of 15th Feb
 veraison_end = 2538 #approximately end of 15th April (start of 16th)
 
 data_dir = "./Interpolated/"
-save_dir = "./DH25Test/"
+save_dir = "./DH25/"
 
-test_file = data_dir + "int_047053.csv"
-save_file = save_dir + "test.csv"
+station = []
+dh25p = []
 
-temp = np.loadtxt(test_file, delimiter = ",")[veraison_start:veraison_end + 1]
+for files in os.listdir(data_dir):
+    data_file = data_dir + files
+    save_file = save_dir + files
 
-diff25 = []
+    diff25 = []
+    dh = []
 
+    temp = np.loadtxt(data_file, delimiter = ",")[veraison_start:veraison_end + 1]
 
-def calcDH25(temp):
     for i in np.arange(0, veraison_end - veraison_start + 1, 1):
+        dh.append(temp[i, 1])
         if (temp[i,1] - 25 > 0):
             diff25.append(temp[i,1] - 25)
 
-print(calcDH25(temp))
-print(sum(diff25))
+    dh25_total = sum(diff25)
+    dh_total = sum(dh)
+
+    dh25_percent = dh25_total / dh_total
+
+    station.append(files[4:10])
+    dh25p.append(dh25_percent)
+
+station = np.array(station)
+dh25p = np.array(dh25p)
+
+final = np.column_stack((station, dh25p))
+print(final)
+
+final_file = save_dir + "station_dh25.csv"
+print(final_file)
+
+np.savetxt(final_file, final, delimiter = ",", fmt = "%s")
